740464667c87471a5a18c1ed6fc7de233650e015
EC2 Default User <ec2-user@ip-172-31-18-24.eu-west-1.compute.internal>
2021-04-15 10:24:25 +0000

    hotfix

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ updater = Updater(key, use_context=True)
 dispatcher = updater.dispatcher
 
 def getXRP(update:Update, context:CallbackContext):
-    print(r.status_code)
+    r = requests.get("https://api.kraken.com/0/public/Ticker?pair=XRPEUR")
     if r.status_code == requests.codes.ok:
         bot.send_message(chat_id=update.effective_chat.id, text=r.json()["result"]["XXRPZEUR"]["a"][0])
     else:
         bot.send_message(chat_id=update.effective_chat.id, text="I am having trouble fetching the data")
 
 def getBTC(update:Update, context:CallbackContext):
+    r = requests.get("https://api.kraken.com/0/public/Ticker?pair=BTCEUR")
+    print(r.status_code)
     if r.status_code == requests.codes.ok:
         bot.send_message(chat_id=update.effective_chat.id, text=r.json()["result"]["XXBTZEUR"]["a"][0])
     else:
